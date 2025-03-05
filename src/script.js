991ba177de21c605966961450a794f46f0e75dbd
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-03-05 21:36:25 +0900

    [Position] - 3개의 mesh에 positoin.y 를 적용해서 아래로 펼쳤는데, 스크롤을 한다고해서 field of view 가 바뀌는게 아니므로 보이는 mesh 가 달라지지 않아서 따로 보이지는 않는다. 일단 position을 적용한 상태다.

--- src/script.js
+++ src/script.js
@@ -40,6 +40,8 @@ const material = new THREE.MeshToonMaterial({
     gradientMap: gradientTexture,
 })
 
+// Meshes
+const objectsDistance = 4;
 const mesh1 = new THREE.Mesh(
     new THREE.TorusGeometry(1, 0.4, 16, 60),
     material
@@ -54,6 +56,10 @@ const mesh3 = new THREE.Mesh(
     new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
     material
 )
+
+mesh1.position.y = - objectsDistance * 0;
+mesh2.position.y = - objectsDistance * 1;
+mesh3.position.y = - objectsDistance * 2;
 
 scene.add(mesh1, mesh2, mesh3)
 
