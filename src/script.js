a8a38fb45f6b29e7a60fb3f9b5df59b34a986920
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-03-06 08:07:50 +0900

    [Parallax] - camera가 cursor에 따라 움직이도록 해서 보는 관점을 다르게 해서 거리감을 느끼게 해준다. 브라우저 크기와 상관없이 동일한 경험을 주기 위해 window.innerWidth, innerHeight로 clientX, Y 를 나눠준다. 0 ~ 1 이 아니라 -0.5 ~ 0.5 가 되도록 0.5를 빼준다.

--- src/script.js
+++ src/script.js
@@ -126,6 +126,18 @@ window.addEventListener('scroll', () => {
 })
 
 /**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+})
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
